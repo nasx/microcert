0046540734a412e8972dab4574d49499110d480d
Maciej Lecki <maciu@maciu.net>
2026-01-27 14:18:55 -0500

    add support for DNS and IP based SANs

--- microcert.py
+++ microcert.py
@@ -6,6 +6,7 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.x509 import Certificate
 from cryptography.x509.oid import NameOID
 import datetime
+import ipaddress  # Added to distinguish between IP and DNS names
 
 def load_certificate(crt_file):
     data = open(crt_file, 'rb').read()
@@ -18,6 +19,25 @@ def load_private_key(key_file):
 def create_certificate(ca_crt: Certificate, ca_key: Certificate, request_json):
     now = datetime.datetime.now(datetime.UTC)
     new_key = rsa.generate_private_key(public_exponent=65537, key_size=4096)
+
+    # --- Start SAN Changes ---
+    # 1. Retrieve the list from the input JSON, defaulting to empty if not present
+    alt_names_raw = request_json.get('subject_alt_names', [])
+
+    # 2. Ensure Common Name is included in SANs (Best practice for modern browsers)
+    if request_json['common_name'] not in alt_names_raw:
+        alt_names_raw.append(request_json['common_name'])
+
+    san_items = []
+    for name in alt_names_raw:
+        try:
+            # 3. Attempt to parse as an IP address
+            ip_obj = ipaddress.ip_address(name)
+            san_items.append(x509.IPAddress(ip_obj))
+        except ValueError:
+            # 4. Fallback to DNS Name if not a valid IP
+            san_items.append(x509.DNSName(name))
+    # --- End SAN Changes ---
 
     return new_key, x509.CertificateBuilder(
         issuer_name=ca_crt.issuer,
@@ -34,7 +54,8 @@ def create_certificate(ca_crt: Certificate, ca_key: Certificate, request_json):
             x509.NameAttribute(NameOID.COMMON_NAME, request_json['common_name'])
         ])
     ).add_extension(
-        x509.SubjectAlternativeName([x509.DNSName(request_json['common_name'])]),
+        # 5. Add the list of SAN items (DNS+IP) here
+        x509.SubjectAlternativeName(san_items),
         critical=False
     ).sign(
         ca_key,
